583820949c6b6c15938071424cb1d4d991c0f667
Dominic <dominic.webandcrafts@gmail.com>
2020-06-09 18:39:27 +0530

    fix error

--- umd/OwlCarousel.js
+++ umd/OwlCarousel.js
@@ -217,24 +217,6 @@
 			}, this));
 
 
-			$.event.special.touchstart = {
-				setup: function( _, ns, handle ){
-				 if ( ns.includes("noPreventDefault") ) {
-				   this.addEventListener("touchstart", handle, { passive: false });
-				 } else {
-				   this.addEventListener("touchstart", handle, { passive: true }); // updated here
-				}
-			   }
-			  };
-			  $.event.special.touchmove = {
-				 setup: function( _, ns, handle ){
-					if ( ns.includes("noPreventDefault") ) {
-					   this.addEventListener("touchmove ", handle, { passive: false });
-					} else {
-					   this.addEventListener("touchmove ", handle, { passive: true }); // updated here
-					}
-				 }
-			  };
 
 			this.setup();
 			this.initialize();
